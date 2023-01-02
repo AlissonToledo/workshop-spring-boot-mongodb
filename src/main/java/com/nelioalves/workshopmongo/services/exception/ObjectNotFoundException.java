7d2ac3947fdf7b7523088797534d3a3fc37f6d96
Alisson Toledo <alisson.fptoledo.1990@gmail.com>
2023-01-02 02:54:24 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/nelioalves/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.nelioalves.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	public ObjectNotFoundException(String string) {
+		}
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
